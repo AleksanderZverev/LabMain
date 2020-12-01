4dcef8012b0980e654075e7af57ba6c552748900
Aleksander Zverev <aleksander.zverev.lt@gmail.com>
2020-12-01 17:47:18 +0500

    fixes after review

--- MCell.h
+++ MCell.h
@@ -2,15 +2,13 @@
 
 class MCell
 {
-
 public:
 	bool getDown() const { return m_down; }
 	bool getRight() const { return  m_right; }
 
 	friend class Maze;
-private:
-	MCell();
 	
-	bool m_down;
-	bool m_right;
+private:
+	bool m_down = false;
+	bool m_right = false;
 };
